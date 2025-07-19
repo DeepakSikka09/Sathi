e13f10122169f894c0f96fbbe9ae5bb30fc1c468
sikka726 <dipaksikka@gmail.com>
2025-07-19 23:04:05 +0530

    local.properties file is updated

---
+++ imageScannerDemo_library/build/generated/source/buildConfig/debug/com/intsig/imageprocessdemo/BuildConfig.java
@@ -0,0 +1,18 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.intsig.imageprocessdemo;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.intsig.imageprocessdemo";
+  public static final String BUILD_TYPE = "debug";
+  // Field from build type: debug
+  public static final String API_KEY = "dT+NLQIHqljOR1CuidQ62h0a0MVc14JPleTMeBs5usA=";
+  // Field from build type: debug
+  public static final String BASE_URL = "http://13.126.182.187:8446/services/last_mile/v1/";
+  // Field from build type: debug
+  public static final String DB_KEY = "ecom@1234";
+  // Field from build type: debug
+  public static final String ENCRYPTION_KEY = "ecom@1234";
+}
